b104c6bcca2b0725740aba1c357fa8993ddff788
Yellokat <jenova195@gmail.com>
2025-03-25 23:40:47 +0200

    feat : part 12 - exercise 8

--- todo-app/todo-backend/mongo/models/Todo.js
+++ todo-app/todo-backend/mongo/models/Todo.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 const todoSchema = new mongoose.Schema({
   text: String,
   done: Boolean
-})
+}, { versionKey: false })
 
 module.exports = mongoose.model('Todo', todoSchema)
