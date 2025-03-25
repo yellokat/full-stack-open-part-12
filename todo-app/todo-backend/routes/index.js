eb7abd797aefe9209962ade32c42a104a9ca93b5
Yellokat <jenova195@gmail.com>
2025-03-26 00:19:20 +0200

    feat : part 12 - exercise 10

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const redis = require('../redis')
 const configs = require('../util/config')
+const {getAsync, setAsync} = require("../redis");
 
 let visits = 0
 
@@ -14,5 +15,16 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+/* GET statistics of todo creation up to now. */
+router.get('/statistics', async (req, res) => {
+  let currentAddedTodos = await getAsync('added_todos');
+  if (!currentAddedTodos) {
+    // if value doesn't exist, init to 0 first
+    await setAsync('added_todos', 0);
+    currentAddedTodos = 0;
+  }
+  res.send({added_todos: currentAddedTodos});
+})
 
 module.exports = router;
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const {Todo} = require('../mongo')
+const {setAsync, getAsync} = require("../redis");
 const router = express.Router();
 
 /* GET todos listing. */
@@ -8,40 +9,28 @@ router.get('/', async (_, res) => {
   res.send(todos);
 });
 
-// GET a single todo
-router.get('/:id', async (req, res) => {
-  const id = req.params.id;
-  const todo = await Todo.findById(id)
-  if (!todo) {
-    res.status(404)
-  }
-  res.send(todo);
-})
-
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
     done: false
-  })
+  });
+  // increment value in redis by 1
+  let currentAddedTodos = await getAsync('added_todos');
+  if (!currentAddedTodos) {
+    // if value doesn't exist, init to 0 first
+    await setAsync('added_todos', 0);
+    currentAddedTodos = 0;
+  }
+  await setAsync('added_todos', Number(currentAddedTodos) + 1);
+  // return created object
   res.send(todo);
 });
-
-// update a single todo
-router.put('/:id', async (req, res) => {
-  const id = req.params.id;
-  const todo = await Todo.findById(id)
-  if (!todo) {
-    res.status(404)
-  }
-  const updatedTodo = await Todo.findByIdAndUpdate(id, req.body, {new: true})
-  res.send(updatedTodo);
-})
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
+  const {id} = req.params
   req.todo = await Todo.findById(id)
   if (!req.todo) return res.sendStatus(404)
 
@@ -50,18 +39,20 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = req.todo;
+  res.send(todo);
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.put('/', async (req, res) => {// update a single todo
+  const updatedTodo = await Todo.findByIdAndUpdate(req.todo.id, req.body, {new: true})
+  res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
